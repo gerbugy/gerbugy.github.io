326e450092d538f6cc058bc5276553883718e7c9
Sung Min Kang <gerbugy@naver.com>
2018-05-26 00:55:31 +0900

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+function fnNavActive(pageUrl) {
+  if(pageUrl.substr(pageUrl.length - 1) == "/") pageUrl = pageUrl.substr(0, pageUrl.length - 1);
+  if(pageUrl.substr(pageUrl.length - 5) == ".html") pageUrl = pageUrl.substr(0, pageUrl.length - 5);
+  $("ul.navbar-nav > li").each(function() {
+    var li = $(this);
+    var href = li.find("a").attr("href");
+    if(pageUrl && href.indexOf(pageUrl) == 0) {
+      li.addClass("active");
+    } else {
+      li.removeClass("active");
+    }
+  });
+}
