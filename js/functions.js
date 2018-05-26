87c92bed587f2b58e16474df1d55286dbc6c6e18
Sung Min Kang <gerbugy@naver.com>
2018-05-26 18:37:52 +0900

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,8 @@ function activeNav(pageUrl) {
   if(pageUrl.substr(pageUrl.length - 1) == "/") pageUrl = pageUrl.substr(0, pageUrl.length - 1);
   if(pageUrl.substr(pageUrl.length - 5) == ".html") pageUrl = pageUrl.substr(0, pageUrl.length - 5);
   $("ul.navbar-nav > li").each(function() {
+    activeNavItem(pageUrl, $(this));
+    /***
     var li = $(this);
     var href = li.find("a").attr("href");
     if(pageUrl.indexOf(href) == 0) {
@@ -9,5 +11,20 @@ function activeNav(pageUrl) {
     } else {
       li.removeClass("active");
     }
+
+    li.find("ul.dropdown-menu > li").each(function() {
+
+    });
+    ***/
+    
   });
+}
+
+function activeNavItem(pageUrl, li) {
+  var href = li.find("a").attr("href");
+  if(pageUrl.indexOf(href) == 0) {
+    li.addClass("active");
+  } else {
+    li.removeClass("active");
+  }
 }
